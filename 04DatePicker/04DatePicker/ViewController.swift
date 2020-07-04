461698d759772b25fc352165d01837a8a47da953
Ki-su, Kim <rabbitthief@naver.com>
2020-07-04 12:24:29 +0900

    Chapter 04. 데이트 피커 사용해 날자 선택하기

---
+++ 04DatePicker/04DatePicker/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  04DatePicker
+//
+//  Created by Ki-su Kim on 7/4/20.
+//  Copyright © 2020 Ki-su Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let timeSelector: Selector = #selector(ViewController.updateTime)
+    let interval = 1.0
+    var count = 0
+    
+    @IBOutlet var lblCurrentTime: UILabel!
+    @IBOutlet var lblPickerTime: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
+    }
+
+    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+        let datePickerView = sender
+        let formatter = DateFormatter()
+        
+        formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
+        lblPickerTime.text = "선택시간 : " + formatter.string(from: datePickerView.date)
+    }
+    
+    @objc func updateTime() {
+//        lblCurrentTime.text = String(count)
+//        count += 1
+        
+        let date = NSDate()
+        let formatter = DateFormatter()
+        
+        formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
+        lblCurrentTime.text = "현재시간 : " + formatter.string(from: date as Date)
+    }
+}
+
